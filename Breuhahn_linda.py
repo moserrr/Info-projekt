b9ebb0d75e34152e6e9bf061064fcf9a6b1909a8
linda06b <128745921+linda06b@users.noreply.github.com>
2023-05-12 09:46:00 +0200

    Add files via upload

--- Breuhahn_linda.py
+++ Breuhahn_linda.py
@@ -1,1 +1,27 @@
-print("Hallo ich bin Linda und gehöre zum Team")
+import pygame
+
+# Initialisieren Sie Pygame
+pygame.init()
+
+# Definieren Sie die Größe des Fensters
+size = (700, 750)
+
+# Öffnen Sie das Pygame-Fenster mit der Hintergrundfarbe Blau
+screen = pygame.display.set_mode(size)
+screen.fill((255, 192, 203))
+
+# Schleife, um das Fenster geöffnet zu halten
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+
+
+# Festlegen der Schriftart und Größe
+font = pygame.font.SysFont("Arial", 30)
+
+    
+
