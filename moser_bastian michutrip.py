21ef49a4eef1e43b5c5d80e9c6324f24927554f1
moserrr <128815094+moserrr@users.noreply.github.com>
2023-06-16 10:00:15 +0200

    Add files via upload

--- moser_bastian michutrip.py
+++ moser_bastian michutrip.py
@@ -7,7 +7,15 @@ import random
 # Initialize Pygame
 pygame.init()
 
+# Initialize Pygame
+pygame.init()
+
  
+
+# Initialize Mixer
+pygame.mixer.init()
+# Initialize Pygame
+pygame.init()
 
 # Define the size of the screen
 screen_width = 800
@@ -18,9 +26,10 @@ screen_size = (screen_width, screen_height)
 
 # Create the screen
 screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption("Start Button")
+pygame.display.set_caption("Doodle Jump")
 
  
+
 
 # Define colors
 ROSA = (255, 192, 203)
@@ -29,73 +38,22 @@ YELLOW = (255, 255, 0)
 
  
 
-# Define the size and position of the start button
-button_width = 200
-button_height = 100
-button_pos = ((screen_width - button_width) // 2, (screen_height - button_height) // 2)
-
- 
-
-# Define the font and size for the start button text
-font = pygame.font.Font(None, 40)
-
- 
-
-# Define the text for the start button
-text = font.render("Start", True, BLACK)
-
- 
-
-# Define the rectangle for the start button
-button_rect = pygame.Rect(button_pos, (button_width, button_height))
-
- 
-
-# Variable to show or hide the start button
-button_visible = True
-
- 
-
-# Game loop to keep the start screen open
-while button_visible:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            if button_rect.collidepoint(mouse_pos):
-                button_visible = False
-                print("Start button clicked!")
-
- 
-
-    # Clear the previous frame
-    screen.fill(ROSA)
-
- 
-
-    # Draw the start button
-    pygame.draw.rect(screen, YELLOW, button_rect)
-    screen.blit(text, (button_pos[0] + 70, button_pos[1] + 30))
-
- 
-
-    # Update the screen
-    pygame.display.update()
-
- 
-
 # Set up game variables
 platform_width, platform_height = 150, 40
-platform_speed = 1
+platform_speed = 2
 
  
 
 # Load the image
 player_image = pygame.image.load("MicrosoftTeams-image (6).png")
 
+# Load song
+pygame.mixer.music.load("TLiJ9fbFN3gE.128.mp3")
+
  
+
+# Play the song in an endless loop (-1 means loop indefinitely)
+pygame.mixer.music.play(-1)
 
 # Get the size of the image
 player_width, player_height = player_image.get_rect().size
@@ -114,14 +72,14 @@ platforms = []
  
 
 # Number of platforms to create
-num_platforms = 5
+num_platforms = 10
 
  
 
 # Generate random platform positions
 for _ in range(num_platforms):
     x = random.randint(0, screen_width - platform_width)
-    y = random.randint(0, screen_height - platform_height)
+    y = random.randint(0, screen_height)
     platforms.append(pygame.Rect(x, y, platform_width, platform_height))
 
  
@@ -160,14 +118,13 @@ while True:
 
     # Player jumping logic
     if not jumping:
+        player_y += platform_speed
         for platform in platforms:
             if platform.colliderect(pygame.Rect(player_x, player_y, player_width, player_height)):
                 jumping = True
                 jump_count = 10
                 player_y = platform.y - player_height
                 break
-        else:
-            player_y += platform_speed
 
  
 
@@ -201,6 +158,4 @@ while True:
 
     # Update the screen
     pygame.display.update()
-
-
 
